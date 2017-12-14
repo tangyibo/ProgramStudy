232e3af25192d98048cc109871e2eb34c0a2d536
Yibo <tangyibo@golaxy.cn>
2017-12-14 16:07:02 +0800

    modidy example-05.cpp

--- cplusplus/example-05.cpp
+++ cplusplus/example-05.cpp
@@ -1,3 +1,8 @@
+/*
+ * 编译需要C++支持：
+ * g++ -std=c++11 example-05.cpp -o test05
+ */
+
 #include <iostream>
 #include <memory>   //declarations of std::auto_ptr
 #include <utility>  //declarations of std::unique_ptr
@@ -26,14 +31,20 @@ public:
 
 int main(int argc,char *argv[])
 {
+	std::auto_ptr<Person> p0(new Person(1));
+	if(p0.get())
+	{
+		p0->Print();	
+		p0.reset();
+	}
+
 	std::unique_ptr<Person> p1(new Person(1));
 	if(p1.get())
 	{
 		p1->Print();
 	
-		std::unique_ptr<Person> p2=p1;
-		p2->age=2;
-		p2->Print();
+		//std::unique_ptr<Person> p2=p1; #不能编译通过
+		//p2->Print();
 	}
 	
 	return 0;
