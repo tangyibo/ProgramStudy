9f246e01dde7ffaacb929504f33cc9a720345e33
Yibo <tangyibo@golaxy.cn>
2017-12-14 15:00:50 +0800

    python reflect

--- python/reflect/reflect.py
+++ python/reflect/reflect.py
@@ -1,15 +1,17 @@
 # -*-encoding:utf-8-*-
 
 class Cat:
-
+	print 'this is a cat'
 	def __init__(self,name):
 		self._name=name;
+		print 'Construct function'
 	
 	def say(self):
 		print 'I\'m a cat ,my name is %s'%self._name	
 
 if __name__=='__main__':
-	cat=Cat('Jack')
+	cat=eval('Cat("Jack")')
+	print 'object class name is :%s'%cat.__class__  
 	print 'Class Cat propery:'
 	print dir(cat)
 	
@@ -17,4 +19,6 @@ if __name__=='__main__':
 		print 'update name to lucy'
 		setattr(cat,'_name','lucy')
 
-	cat.say()	
+	cat.say()
+
+	c=Cat('twin')	
