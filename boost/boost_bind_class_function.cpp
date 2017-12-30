70c5a515ddcdc7249b39df749a763be75dfb2714
Yibo <inrgihc@126.com>
2017-12-31 00:07:31 +0800

    Update boost_bind_class_function.cpp

--- boost/boost_bind_class_function.cpp
+++ boost/boost_bind_class_function.cpp
@@ -1,23 +1,34 @@
 #include<iostream>
 #include<boost/bind.hpp>
+#include<boost/function.hpp>
 #include<boost/typeof/typeof.hpp>
 #include<string>
 
-class Test
+int print(int x,std::string str)
 {
-public:
-	int print(int x,std::string str)
+	std::cout<<"x="<<x<<",str="<<str<<std::endl;
+	return 0;
+}
+
+struct Tester
+{
+	int operator()(int x,const char* str)
 	{
 		std::cout<<"x="<<x<<",str="<<str<<std::endl;
-		return 0;
 	}
 };
 
 int main(int argc,char *argv[])
 {
-	Test t;
-	BOOST_AUTO(fun,boost::bind(&Test::print,&t,332,_1));
-	fun("hello,world!");
+	BOOST_AUTO(fun1,boost::bind(print,1,_1));
+        fun1("hello!");
+
+	boost::function<int(int,std::string)> f=print;
+	BOOST_AUTO(fun2,boost::bind(f,2,_1));
+	fun2("world!");
+
+	BOOST_AUTO(fun3,boost::bind<int>(Tester(),_1,_2));
+	fun3(3,"test!");
 
 	return 0;
 }
