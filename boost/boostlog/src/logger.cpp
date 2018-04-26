bdf4975b5790cafe52216d0eab44fb3d14a8e75a
Yibo <inrgihc@126.com>
2018-04-26 17:50:28 +0800

    Add files via upload

---
+++ boost/boostlog/src/logger.cpp
@@ -0,0 +1,36 @@
+#include "logger.h"
+#include <fstream>
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/from_stream.hpp>
+#include <boost/log/utility/setup/formatter_parser.hpp>
+#include <boost/log/utility/setup/filter_parser.hpp>
+
+bool init_log_environment(std::string _cfg)
+{
+    namespace logging = boost::log;
+    using namespace logging::trivial;
+
+    if (!boost::filesystem::exists("./log/"))
+    {
+        boost::filesystem::create_directory("./log/");
+    }
+    logging::add_common_attributes();
+
+    logging::register_simple_formatter_factory<severity_level, char>("Severity");
+    logging::register_simple_filter_factory<severity_level, char>("Severity");
+
+    std::ifstream file(_cfg.c_str());
+    try
+    {
+        logging::init_from_stream(file);
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "init_logger is fail, read log config file fail. curse: " << e.what() << std::endl;
+        exit(-2);
+    }
+    return true;
+}
---
+++ boost/boostlog/src/logger.h
@@ -0,0 +1,9 @@
+#ifndef _LOGGER_H_
+#define _LOGGER_H_ 
+#include <string>
+
+#define BOOST_LOG_DYN_LINK 1
+
+bool init_log_environment(std::string _cfg);
+
+#endif
